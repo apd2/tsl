aa9de4687c4d7cd47ccd15a82ba248fba3085cb3
Leonid Ryzhyk <leonidr@cse.unsw.edu.au>
2012-10-08 16:40:30 -0400

    sketching data structures for the predicate update algorithm

--- internal/Predicate.hs
+++ internal/Predicate.hs
@@ -1,73 +1,142 @@
+{-# LANGUAGE ImplicitParams #-}
+
 module Predicate() where
 
+import Common
+import ISpec
+
+-- Arithmetic operations
+data ArithUOp = AUMinus 
+              | ABNeg
+              deriving (Eq)
+
+data ArithBOp = ABAnd 
+              | ABOr 
+              | ABXor
+              | APlus 
+              | ABinMinus 
+              | AMod
+              | AMul
+              deriving(Eq)
+
+-- Arithmetic term
+data Term = TVar    String
+          | TInt    Integer
+          | TEnum   String
+          | TTrue
+          | TAddr   Term
+          | TField  Term String
+          | TIndex  Term Term
+          | TUnOp   ArithUOp Term
+          | TBinOp  ArithBOp Term Term
+          | TSlice  Term (Term,Term)
+
+-- Relational operations
+data RelOp = REq 
+           | RNeq 
+           | RLt 
+           | RGt 
+           | RLte 
+           | RGte
+           deriving (Eq)
+
+bopToRelOp :: BOp -> RelOp
+bopToRelOp Eq  = REq
+bopToRelOp Neq = RNeq
+bopToRelOp Lt  = RLt
+bopToRelOp Gt  = RGt
+bopToRelOp Lte = RLte
+bopToRelOp Gte = RGte
+
+-- Predicates
 data Predicate = PAtom {pOp :: RelOp, p1 :: Term, p2 :: Term}
 
-data Term = TVar    String
-            TConst  Val
-          | EField  Expr String
-          | EIndex  Expr Expr
-          | EUnOp   UOp Expr
-          | EBinOp  BOp Expr Expr
-          | ESlice  Expr Slice
-          | EStruct String [Expr]
-
-
-         | IntVal    Integer
-         | StructVal (M.Map String TVal)
-         | EnumVal   String
-         | PtrVal    LExpr
-         | ArrayVal  [TVal]
-
-
-pAtom :: RelOp -> Expr -> Expr -> Predicate
-pAtom op l r = norm $ Predicate op l r
-
-data RelOp = REq
-
-data Formula = FBin BoolOp Formula Formula
-             | FReplace  Predicate Formula
-             | FPred     Predicate
-             | FTrue
-             | FFalse
-
-data BoolOp = Conj | Disj | Impl
+pAtom :: RelOp -> Term -> Term -> Predicate
+pAtom op l r = norm $ PAtom op l r
 
 -- Objects with canonical form
 class Canonical a where
     norm :: a -> a
 
-instance Canonical Predicate
+instance Canonical Predicate where
 
--- Convert _boolean_ expression to a formula
+-- Predicate database
+type PredicateDB = [Predicate]
+
+-- Logical operations
+data BoolOp = Conj 
+            | Disj 
+            | Impl
+            | Equiv
+            deriving (Eq)
+
+-- Formula consists of predicates and boolean constants
+-- connected with boolean connectors
+data Formula = FTrue
+             | FFalse
+             | FPred     Predicate
+             | FBinOp    BoolOp Formula Formula
+             | FNot      Formula
+             | FReplace  Predicate Formula
+
+
+-- Intermediate data structure that represents the value of
+-- an arithmetic expression depending on pointer predicates
+-- or other conditions
+data Cascade = CasCase [(Formula, Cascade)] 
+             | CasTerm Expr
+
+ctrue :: Cascade
+ctrue = CasTerm TTrue
+
+-- Convert arithmetic expression to arithmetic term
+exprExpandPtr :: (?pdb::PredicateDB) => Expr -> Cascade
+exprExpandPtr e = error "Not implemented: exprExpandPtr" 
+
+-- Compare two cascades
+combine :: RelOp -> Cascade -> Cascade -> Formula
+combine op c1 c2 = error "Not implemented: combine"
+
+-- op is a relational operator
+--combineExpr :: BOp -> Expr -> Expr -> Formula
+--combineExpr op e1 e2
+-- case 1: both sides are boolea atoms
+-- case 2: one of the sides is a more complex boolean expression
+
+
+-- Convert boolean expression to a formula
 exprToFormula :: (?pdb::PredicateDB) => Expr -> Formula
+exprToFormula e@(EVar   _)             = combine REq (exprExpandPtr e) ctrue
+exprToFormula e@(EField _ _)           = combine REq (exprExpandPtr e) ctrue
+exprToFormula e@(EIndex _ _)           = combine REq (exprExpandPtr e) ctrue
 exprToFormula (EConst (BoolVal True))  = FTrue
 exprToFormula (EConst (BoolVal False)) = FFalse
 exprToFormula (EUnOp  Not e)           = FNot $ exprToFormula e
 exprToFormula (EBinOp op e1 e2) | elem op [Eq, Neq, Lt, Gt, Lte, Gte] = 
-    combine op (expandPtr e1) (expandPtr e2)
-exprToFormula (EBinOp And e1 e2)       = FBin Conj (exprToFormula e1) (exprToFormula e2)
-exprToFormula (EBinOp Or e1 e2)        = FBin Disj (exprToFormula e1) (exprToFormula e2)
-exprToFormula (EBinOp Imp e1 e2)       = FBin Impl (exprToFormula e1) (exprToFormula e2)
+                                         combine (bopToRelOp op) (exprExpandPtr e1) (exprExpandPtr e2)
+exprToFormula (EBinOp And e1 e2)       = FBinOp Conj (exprToFormula e1) (exprToFormula e2)
+exprToFormula (EBinOp Or e1 e2)        = FBinOp Disj (exprToFormula e1) (exprToFormula e2)
+exprToFormula (EBinOp Imp e1 e2)       = FBinOp Impl (exprToFormula e1) (exprToFormula e2)
 
-
-expandPtr :: Expr -> Cascade
-
-combine :: BOp -> Cascade -> Cascade -> Formula
-
--- op is a relational operator
-combineExpr :: BOp -> Expr -> Expr -> Formula
-combineExpr op e1 e2
--- case 1: both sides are boolea atoms
--- case 2: one of the sides is a more complex boolean expression
-
-type Cascade = Either [(Formula, Cascade)] Expr
-
-
-exprToFormula e = exprToFormula $ EBinOp Eq e true
-
+-- Weakest precondition of a formula wrt a statement
 wps :: Formula -> Statement -> Formula
-wps f (SAssume e) = FAnd f (exprToFormula e)
+wps f (SAssume e)     = FBinOp Conj f (exprToFormula e)
 wps f (SAssign e1 e2) = 
 
 
-wpt :: (?spec::Spec) => Formula -> Transition
+--wp(e1 op e2, e3:=e4)  = wp(e1, e3:=e4)  op wp(e2, e3:=e4)
+--
+--wp(e, e3:=e4) -> -- find which part of e might be changed and
+--                 -- to what subexpressions of e4
+--                 overlap(e, e3, e4)::Cascade Expr
+--                 -- for each element in the cascade,
+--                 -- expand the RHS
+--                 rhs -> 
+--
+--combinerhs :: (Expr, Expr) -> Cascade Formula
+---- expand pointers in order
+--
+
+---- Weakest precondition of a formula wrt a transition
+--wpt :: (?spec::Spec) => Formula -> Transition -> Formula
+--
